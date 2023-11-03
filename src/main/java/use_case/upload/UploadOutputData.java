2321d671fcc74e51dd01e3e11b97b260fc9ca6a6
jchionak <jchionak@icloud.com>
2023-11-03 16:45:39 -0400

    changed output data back

--- src/main/java/use_case/upload/UploadOutputData.java
+++ src/main/java/use_case/upload/UploadOutputData.java
@@ -3,9 +3,15 @@ package use_case.upload;
 import entities.Episode;
 
 public class UploadOutputData {
+    private final Episode episode;
     private final boolean useCaseFailed;
-    public UploadOutputData(boolean useCaseFailed) {
+    public UploadOutputData(Episode episode, boolean useCaseFailed) {
+        this.episode = episode;
         this.useCaseFailed = useCaseFailed;
+    }
+
+    public Episode getEpisode() {
+        return episode;
     }
 
     public boolean getUseCaseFailed() {
