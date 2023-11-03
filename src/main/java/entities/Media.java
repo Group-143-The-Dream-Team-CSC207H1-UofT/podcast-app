08d168c4022e0eba5ea49ef6db4e4db23d25e73b
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-11-03 15:04:19 -0400

    Implemented Media entity

---
+++ src/main/java/entities/Media.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.util.List;
+
+public abstract class Media {
+    private int id;
+    private String mediaName;
+    private List<MediaItem> mediaItems;
+
+    // Constructors, getters ...
+}
+
