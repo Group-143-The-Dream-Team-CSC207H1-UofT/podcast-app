34b683844085c512420260d13cdf58dfa2d89203
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-11-03 15:06:02 -0400

    Implemented MediaItem entity

---
+++ src/main/java/entities/MediaItem.java
@@ -0,0 +1,10 @@
+package entities;
+
+public abstract class MediaItem {
+    private int id;
+    private String itemName;
+    private String itemDescription;
+
+    // constructor and getters etc...
+}
+
