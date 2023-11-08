3944803b35c94b3a7d81b0b7f482930110d2ecfc
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-11-07 23:03:06 -0500

    implement input boundary and output boundary for transcribe use case

---
+++ src/main/java/use_case/transcribe/TranscribeInputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.transcribe;
+
+import java.io.IOException;
+
+public interface TranscribeInputBoundary {
+    public void execute(TranscribeInputData inputdata);
+}
---
+++ src/main/java/use_case/transcribe/TranscribeOutputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.transcribe;
+
+public interface TranscribeOutputBoundary {
+    void prepareSuccessView(TranscribeOutputData transcribeOutputData);
+
+    void prepareFailView(String error);
+}
