bdf49d0a8c10604bb56c3cd2050a2ca1b8d86945
Ali Shabani <shabaniali1346@gmail.com>
2023-11-12 13:51:35 -0500

    clean up UploadUseCaseFactory

--- src/main/java/app/UploadUseCaseFactory.java
+++ src/main/java/app/UploadUseCaseFactory.java
@@ -1,21 +1,21 @@
 package app;
 
-        import data_access.EpisodeDataAccess;
-        import data_access.TranscriptDataAccess;
-        import interface_adapter.ViewManagerModel;
-        import interface_adapter.transcribe.TranscribeController;
-        import interface_adapter.transcribe.TranscribePresenter;
-        import interface_adapter.transcribe.TranscribeViewModel;
-        import interface_adapter.upload.*;
-        import use_case.transcribe.TranscribeInputBoundary;
-        import use_case.transcribe.TranscribeInteractor;
-        import use_case.transcribe.TranscribeOutputBoundary;
-        import use_case.upload.*;
-        import view.UploadView;
+import data_access.EpisodeDataAccess;
+import data_access.TranscriptDataAccess;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.transcribe.TranscribeController;
+import interface_adapter.transcribe.TranscribePresenter;
+import interface_adapter.transcribe.TranscribeViewModel;
+import interface_adapter.upload.*;
+import use_case.transcribe.TranscribeInputBoundary;
+import use_case.transcribe.TranscribeInteractor;
+import use_case.transcribe.TranscribeOutputBoundary;
+import use_case.upload.*;
+import view.UploadView;
 
 
 public class UploadUseCaseFactory {
-
+        
     public static UploadView create(ViewManagerModel viewManagerModel, UploadViewModel uploadViewModel, TranscribeViewModel transcribeViewModel, EpisodeDataAccess episodeDataAccess, TranscriptDataAccess transcriptDataAccess) {
         UploadController uploadController = createUploadUseCase(viewManagerModel, uploadViewModel, episodeDataAccess);
         TranscribeController transcribeController = createTranscribeUseCase(viewManagerModel, transcribeViewModel, episodeDataAccess, transcriptDataAccess);
@@ -27,6 +27,7 @@ public class UploadUseCaseFactory {
         UploadInputBoundary uploadInteractor = new UploadInteractor(uploadOutputBoundary, episodeDataAccess);
         return new UploadController(uploadInteractor);
     }
+        
     private static TranscribeController createTranscribeUseCase(ViewManagerModel viewManagerModel, TranscribeViewModel transcribeViewModel, EpisodeDataAccess episodeDAO, TranscriptDataAccess transcriptDAO) {
         TranscribeOutputBoundary transcribeOutputBoundary = new TranscribePresenter(transcribeViewModel, viewManagerModel);
         TranscribeInputBoundary transcribeInteractor = new TranscribeInteractor(transcribeOutputBoundary, episodeDAO, transcriptDAO);
