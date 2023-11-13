a31fef5e4c167441c74cb89273fbb1d0ee354bdf
sukhjeetsingh <ssingh.0471@gmail.com>
2023-11-13 16:48:23 -0500

    updated attributes and added methods to users entity (for user DAO)

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,19 +1,40 @@
 package entities;
 import java.util.List;
+import java.util.UUID;
 
 public class User {
-    private int id;
-    private String username;
-    private String password;
-    private String email;
-    private List<Podcast> podcastsCreated;
+    private final UUID id;
+    private final String username;
+    private final String password;
+    private final String email;
+    private List<Podcast> userPodcasts;
 
-    public User(int id, String username, String password, String email, List<Podcast> podcastsCreated) {
+    public User(UUID id, String username, String password, String email, List<Podcast> userPodcasts) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
-        this.podcastsCreated = podcastsCreated;
+        this.userPodcasts = userPodcasts;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Podcast> getUserPodcasts() {
+        return userPodcasts;
     }
 
 }
