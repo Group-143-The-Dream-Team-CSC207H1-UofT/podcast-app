2b0ffb06746d946e1da44fda1d22ccb4dcda4f9a
David De Martin <david.demartin123@gmail.com>
2023-11-14 10:23:59 -0500

    Create api objects for search index use case

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,28 +2,33 @@ package app;
 
 import javax.swing.JFrame;
 
+import api.EmbeddingsInterface;
+import api.OpenAIEmbeddings;
 import api.WhisperTranscription;
-import data_access.EpisodeDataAccess;
-import data_access.EpisodeDataAccessObject;
-import data_access.TranscriptDataAccess;
-import data_access.TranscriptDataAccessObject;
+import data_access.*;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.transcribe.TranscribeViewModel;
 import interface_adapter.upload.*;
 import view.*;
 
+import java.util.Vector;
+
 public class Main {
 
     public static void main(String[] args) {
         String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+        String PINECONE_API_KEY = System.getenv("PINECONE_API_KEY");
+        String PINECONE_BASE_URL = System.getenv("PINECONE_BASE_URL");
         WhisperTranscription transcriptionObject = new WhisperTranscription(OPENAI_API_KEY);
+        EmbeddingsInterface embeddings = new OpenAIEmbeddings(OPENAI_API_KEY);
+        VectorDatabase vectorDatabase = new PineconeVectorDatabase(PINECONE_API_KEY, PINECONE_BASE_URL);
 
         TranscriptDataAccess transcriptDataAccessObject = new TranscriptDataAccessObject();
         EpisodeDataAccess episodeDataAccessObject = new EpisodeDataAccessObject(transcriptDataAccessObject);
         UploadViewModel uploadViewModel = new UploadViewModel();
         TranscribeViewModel transcribeViewModel = new TranscribeViewModel();
         ViewManagerModel viewManagerModel = new ViewManagerModel();
-        UploadView uploadView = UploadUseCaseFactory.create(viewManagerModel, uploadViewModel, transcribeViewModel, episodeDataAccessObject, transcriptDataAccessObject, transcriptionObject);
+        UploadView uploadView = UploadUseCaseFactory.create(viewManagerModel, uploadViewModel, transcribeViewModel, episodeDataAccessObject, transcriptDataAccessObject, transcriptionObject, vectorDatabase, embeddings);
 
         // Set up the main window (a JFrame)
         JFrame frame = new JFrame("Upload Podcast");
