7824cfd37d743d0cbf06ca3fa8a5cca6b2ae5efb
David De Martin <david.demartin123@gmail.com>
2023-11-16 12:50:10 -0500

    Add search use case interactor

---
+++ src/main/java/use_case/search/SearchInteractor.java
@@ -0,0 +1,42 @@
+package use_case.search;
+
+import api.EmbeddingsInterface;
+import data_access.EpisodeDataAccess;
+import data_access.VectorDatabase;
+import entities.Episode;
+import entities.TextChunk;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class SearchInteractor implements SearchInputBoundary {
+    private final VectorDatabase vectorDatabase;
+    private final EmbeddingsInterface embeddingsGenerator;
+    private final EpisodeDataAccess episodeDAO;
+    private final SearchOutputBoundary outputBoundary;
+
+    public SearchInteractor(SearchOutputBoundary outputBoundary, VectorDatabase vectorDatabase, EmbeddingsInterface embeddings, EpisodeDataAccess episodeDAO) {
+        this.outputBoundary = outputBoundary;
+        this.vectorDatabase = vectorDatabase;
+        this.embeddingsGenerator = embeddings;
+        this.episodeDAO = episodeDAO;
+    }
+
+    public void execute(String query) {
+        float[] vector = embeddingsGenerator.getEmbedding(query);
+        List<String> resultIds = vectorDatabase.query(vector);
+        List<SearchResult> searchResults = new ArrayList<>();
+        for (String resultId : resultIds) {
+            String[] parts = resultId.split("\\+");
+            UUID episodeId = UUID.fromString(parts[0]);
+            Episode episode = episodeDAO.getEpisodeById(episodeId);
+            if (parts.length == 1) {
+                searchResults.add(new EpisodeSearchResult(episode));
+            } else {
+                TextChunk textChunk = episode.getTranscript().getTextChunks().get(Integer.parseInt(parts[1]));
+                searchResults.add(new TextChunkSearchResult(episode, textChunk));
+            }
+        }
+        outputBoundary.prepareSuccessView(new SearchOutputData(searchResults, false));
+    }
+}
