f0174efe0ee3fe832f88a78ba151479dd9c333d9
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-11-18 16:41:46 -0500

    add documentation for transcript data access object

--- src/main/java/data_access/TranscriptDataAccess.java
+++ src/main/java/data_access/TranscriptDataAccess.java
@@ -4,9 +4,32 @@ import entities.Transcript;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * Interface defining data access operations for transcripts.
+ */
 public interface TranscriptDataAccess {
+
+    /**
+     * Saves a transcript to the data store.
+     *
+     * @param transcript The transcript to save.
+     * @return true if the transcript was saved successfully, false otherwise.
+     */
     public boolean saveTranscript(Transcript transcript);
 
+    /**
+     * Retrieves a transcript by its unique identifier.
+     *
+     * @param id The UUID of the transcript.
+     * @return The Transcript object associated with the ID, or null if not found.
+     */
     public Transcript getTranscriptById(UUID id);
+
+    /**
+     * Converts a string representation of a transcript into a list of text chunks.
+     *
+     * @param content The string content of the transcript.
+     * @return A list of TextChunk objects representing the content.
+     */
     public List<TextChunk> stringToChunks(String content);
 }
