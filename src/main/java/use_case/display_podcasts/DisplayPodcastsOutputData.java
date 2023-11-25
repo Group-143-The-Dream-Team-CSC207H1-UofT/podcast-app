07bcb3ae63a24afcdb7f770fa40286a457fb26df
jchionak <jchionak@icloud.com>
2023-11-24 23:18:40 -0500

    add error to DisplayPodcastsOutputData

--- src/main/java/use_case/display_podcasts/DisplayPodcastsOutputData.java
+++ src/main/java/use_case/display_podcasts/DisplayPodcastsOutputData.java
@@ -7,14 +7,21 @@ import java.util.List;
 
 public class DisplayPodcastsOutputData {
     private final List<Podcast> allPodcasts;
+    private String error;
     private final boolean useCaseFailed;
-    public DisplayPodcastsOutputData(List<Podcast> allPodcasts, boolean useCaseFailed) {
+    public DisplayPodcastsOutputData(List<Podcast> allPodcasts, String error, boolean useCaseFailed) {
         this.allPodcasts = allPodcasts;
+        this.error = error;
         this.useCaseFailed = useCaseFailed;
     }
     public List<Podcast> getAllPodcasts() {
         return allPodcasts;
     }
+
+    public String getError() {
+        return error;
+    }
+
     public boolean isUseCaseFailed() {
         return useCaseFailed;
     }
