2d6c8f1add69df1e70963588d7e3f8bc6c6b8054
jchionak <jchionak@icloud.com>
2023-11-24 23:20:28 -0500

    create DisplayPodcastsView

---
+++ src/main/java/view/DisplayPodcastsView.java
@@ -0,0 +1,81 @@
+package view;
+
+import entities.Podcast;
+import interface_adapter.display_podcasts.DisplayPodcastsController;
+import interface_adapter.display_podcasts.DisplayPodcastsState;
+import interface_adapter.display_podcasts.DisplayPodcastsViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+
+public class DisplayPodcastsView extends JFrame implements PropertyChangeListener {
+    private JPanel mainPanel;
+    private JButton searchButton;
+    private JButton createButton;
+    private JPanel secondaryPanel;
+    private final DisplayPodcastsController controller;
+    private final DisplayPodcastsViewModel viewModel;
+
+    public DisplayPodcastsView(DisplayPodcastsController controller, DisplayPodcastsViewModel viewModel) {
+        this.controller = controller;
+        this.viewModel = viewModel;
+        this.viewModel.addPropertyChangeListener(this);
+        controller.execute();
+        setTitle("Home");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 600);
+        setLocationRelativeTo(null);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Going to search view...");
+            }
+        });
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Going to podcast view...");
+            }
+        });
+        DisplayPodcastsState state = this.viewModel.getState();
+        secondaryPanel.setLayout(new GridLayout(state.getAllPodcasts().size(), 1));
+        List<Podcast> allPodcasts = state.getAllPodcasts();
+        for (Podcast podcast: allPodcasts) {
+            JButton podcastButton = new JButton(podcast.getName());
+            System.out.println(podcast.getName());
+            podcastButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Go to podcast view...");
+                }
+            });
+            secondaryPanel.add(podcastButton);
+        }
+        setContentPane(mainPanel);
+        setVisible(true);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+//        DisplayPodcastsState state = (DisplayPodcastsState) evt.getNewValue();
+//        // I'm not really sure what needs to happen here, this view will be mostly static, it should change when there
+//        // are new podcasts uploaded only
+//        secondaryPanel.removeAll();
+//        List<Podcast> allPodcasts = state.getAllPodcasts();
+//        for (Podcast podcast: allPodcasts) {
+//            JButton podcastButton = new JButton(podcast.getName());
+//            podcastButton.addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//                    System.out.println("Go to podcast view...");
+//                }
+//            });
+//            secondaryPanel.add(podcastButton);
+//        }
+    }
+}
